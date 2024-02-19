d780d8e0960ad6074aa40141d11b1eeaa6e9537a
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-02-19 16:48:30 +0600

    Add files via upload

---
+++ StudentId/src/studentid/StudentId.java
@@ -0,0 +1,19 @@
+
+package studentid;
+
+
+public class StudentId {
+
+    String name;
+    int id=0;
+   
+    public static void main(String[] args) {
+        StudentId ob=new StudentId();
+        ob.name="TAMIM";
+        ob.id=19411682;
+        System.out.println("STUDENT NAME IS: "+ob.name);
+        System.out.println("STUDENT NAME IS: "+ob.id);
+     
+    }
+    
+}
