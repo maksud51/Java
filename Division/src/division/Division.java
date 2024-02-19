d4525839f6555e642f7d1fbb6bb4b9b20eed8168
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-02-19 16:31:32 +0600

    Add files via upload

---
+++ Division/src/division/Division.java
@@ -0,0 +1,29 @@
+
+package division;
+import java.util.Scanner;
+
+class Division {
+  public static void main(String[] args) {
+ 
+  int a, b, result;
+ 
+  Scanner input = new Scanner(System.in);
+  System.out.println("Input two integers");
+ 
+  a = input.nextInt();
+  b = input.nextInt();
+ 
+
+ 
+  try {
+    result  = a / b;
+    System.out.println("Result = " + result);
+  }
+ 
+  
+ 
+  catch (ArithmeticException e) {
+    System.out.println("Exception caught: Division by zero.");
+  }
+  }
+}
