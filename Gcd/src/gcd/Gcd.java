403397df638527bcba6e2af13b94ce3cc815c71c
Md Al Maksud Alam <91654429+maksud51@users.noreply.github.com>
2024-02-19 16:37:04 +0600

    Add files via upload

---
+++ Gcd/src/gcd/Gcd.java
@@ -0,0 +1,15 @@
+
+package gcd;
+
+public class Gcd {
+
+
+    public static void main(String[] args) {
+   System.out.println(" I LOVE BANGLADESH");
+   System.out.print("NO ONES LOVE GOVERMENT");
+    }
+}
+
+   
+    
+
